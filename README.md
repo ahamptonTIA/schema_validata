f511021b9b721f82bb9458c433fb32ef1ae2cdeb
AHampton NTIA <115647832+ahamptonTIA@users.noreply.github.com>
2024-02-16 12:16:49 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# schema_validata
+python package to validate data set structures/schemas against a defined data dictionary.
---
+++ setup.py
@@ -0,0 +1,38 @@
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='schema_validata',                 # name of the package
+    version='0.0.1',                        # release version
+    author='ahamptonTIA',                   # org/author
+    description=\
+        '''
+        databricks_helper
+
+        The schema_validata package is a collection of functions
+        to check datasets for data complinace based on a given an xlsx
+        data dictionary (see template).   
+         
+        ''',
+    long_description=long_description,      # long description read from the the readme file
+    long_description_content_type='text/markdown',
+    classifiers=[                           # information to filter the project on PyPi website
+                        'Programming Language :: Python :: 3',
+                        'License :: OSI Approved :: MIT License',
+                        'Operating System :: OS Independent',
+                        'Natural Language :: English',
+                        'Programming Language :: Python :: 3.7',
+                        ],                                      
+    python_requires='>=3.7',                # minimum version requirement of the package
+    py_modules=['schema_validata'],         # name of the python package     
+    package_dir={'':'src'},                 # directory of the source code of the package
+    packages=setuptools.find_packages(where="src"), # list of all python modules to be installed
+    install_requires=[           # package dependencies                
+                     'pandas>=1.0',
+                     'openpyxl',
+
+                    ]
+    )
