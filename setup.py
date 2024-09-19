9030b0a7558ff9940f517474109323aed8752609
AHampton NTIA <115647832+ahamptonTIA@users.noreply.github.com>
2024-09-19 11:05:08 -0500

    Update setup.py
    Signed-off-by: AHampton NTIA <115647832+ahamptonTIA@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
                         'numpy<1.25,>=1.16.0',
                         'openpyxl',
                         'sqlparse', 
-                        'sqlite3'
+                        #'sqlite3'
 
                     ]
     )
