a871b11b19cc59cbd28c57750b8012b88f05b0e4
ahamptonTIA <ahamptonTIA>
2024-10-10 11:58:27 +0000

    test

--- src/schema_validata.py
+++ src/schema_validata.py
@@ -2988,10 +2988,12 @@ def get_rows_with_condition_spark(tables, sql_statement, error_message, error_le
             #                 """
 
             modified_sql = f"""
-                            SELECT
-                                {F.col(f"sq.{unique_column}")} AS Lookup_Value
-                            FROM ({sql_statement}) AS sq
-                        """
+                SELECT
+                    {F.col(f"sq.{unique_column}")} AS Lookup_Value
+                FROM (
+                    {sql_statement}
+                ) AS sq
+            """
 
             # Register the primary table as a temporary view
             primary_df.createOrReplaceTempView("primary_table")   
@@ -3148,7 +3150,7 @@ def find_errors_with_sql(data_dict_path, files, sheet_name=None):
     # Extract table references from each SQL rule
     for index, row in rules_df.iterrows():
         sql_statement = row['SQL Error Query']
-        sql_ref_tables.append(extract_primary_table(sql_statement))
+        sql_ref_tables.append(extract_primary_table(sql_statement)
         sql_ref_tables.extend(extract_all_table_names(sql_statement)) 
     sql_ref_tables = list(set(sql_ref_tables))
     print(f'Loading tables: {sql_ref_tables}')
