e4c618ac81ae3c39418ca8c880a339d52b77abba
ahamptonTIA <ahamptonTIA>
2024-10-10 00:37:25 +0000

    test

--- src/schema_validata.py
+++ src/schema_validata.py
@@ -2979,23 +2979,14 @@ def get_rows_with_condition_spark(tables, sql_statement, error_message, error_le
             else:
                 unique_column = get_best_uid_column(primary_df.pandas_api())
 
-            # Modify the SQL statement to select the unique ID column
-            # modified_sql = f"""
-            #                 SELECT 
-            #                     pt.{unique_column} AS Lookup_Value
-            #                 FROM ({sql_statement}) AS sq
-            #                 LEFT JOIN primary_table pt ON sq.{unique_column} = pt.{unique_column}
-            #                 """
-            if 't1.' in sql_statement:
-                sq_alias = 't1.'
-            else: 
-                sq_alias = ''
+            Modify the SQL statement to select the unique ID column
             modified_sql = f"""
                             SELECT 
                                 pt.{unique_column} AS Lookup_Value
                             FROM ({sql_statement}) AS sq
-                            LEFT JOIN primary_table pt ON sq.{sq_alias}{unique_column} = pt.{unique_column}
+                            LEFT JOIN primary_table pt ON sq.{unique_column} = pt.{unique_column}
                             """
+
             # Register the primary table as a temporary view
             primary_df.createOrReplaceTempView("primary_table")   
                     
